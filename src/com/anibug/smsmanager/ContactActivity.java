ed27ff8cf8e0342ee35a0adb857d1886ad53de8e
Harry gao <shadowpriest.kya@gmail.com>
2011-09-02 13:53:40 +0800

    merge ContactPickerManager into ContactManager

--- src/com/anibug/smsmanager/ContactActivity.java
+++ src/com/anibug/smsmanager/ContactActivity.java
@@ -2,14 +2,19 @@ package com.anibug.smsmanager;
 
 import java.util.List;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.text.style.LineHeightSpan.WithDensity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.BaseAdapter;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.anibug.smsmanager.adapter.ContactArrayAdapter;
 import com.anibug.smsmanager.model.Contact;
@@ -17,6 +22,8 @@ import com.anibug.smsmanager.model.ContactManager;
 
 public class ContactActivity extends ListActivity {
 	ContactManager contactManager;
+	
+	private static final int PICK_CONTACT_RESULT = 1001;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -38,10 +45,41 @@ public class ContactActivity extends ListActivity {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		ShowEditingDialog();
+		switch (item.getItemId()) {
+		case R.id.item_new_contact:
+			ShowEditingDialog();
+			break;
+		case R.id.item_pick_contact:
+			ShowPickContactDialog();
+			break;
+		default:
+			break;
+		}
+		
 		return true;
 	}
 	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+		super.onActivityResult(requestCode, resultCode, data);
+
+		if (requestCode == PICK_CONTACT_RESULT) {
+			if (resultCode == Activity.RESULT_OK) {
+
+				Contact picked = contactManager.getContactFromPickResult(data.getData());
+				Toast.makeText(this, "Select "+ picked.getName() + " with number: " + picked.getPhoneNumber(), Toast.LENGTH_SHORT).show();
+			}
+		}
+	}
+
+	private void ShowPickContactDialog() {
+		  Intent intent = new Intent(Intent.ACTION_PICK);
+		  intent.setType(ContactsContract.Contacts.CONTENT_TYPE);
+		  startActivityForResult(intent, PICK_CONTACT_RESULT);
+		
+	}
+
 	private void ShowEditingDialog() {
 		final EditText edit = new EditText(this);
 
--- src/com/anibug/smsmanager/model/ContactManager.java
+++ src/com/anibug/smsmanager/model/ContactManager.java
@@ -4,9 +4,13 @@ import java.util.Formatter;
 import java.util.HashSet;
 import java.util.Set;
 
+import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
+import android.util.Log;
 
 import com.anibug.smsmanager.model.Contact.DataBase;
 import com.anibug.smsmanager.model.filter.Filter;
@@ -15,8 +19,11 @@ import com.anibug.smsmanager.model.filter.Filter;
 public class ContactManager extends Manager<Contact> implements Filter {
 	public ContactManager(Context context) {
 		super(context);
+		
+		mContext = context;
 	}
-
+	
+	Context mContext;
 	public static final int MIN_PHONENUMBER_LENGTH_IN_CHINA = 11;
 	
 	private Set<String> phoneNumbers = null;
@@ -94,5 +101,48 @@ public class ContactManager extends Manager<Contact> implements Filter {
 
 	public boolean match(Message message) {
 		return hasPhoneNumber(message.getPhoneNumber());
+	}
+	
+	public Contact getContactFromPickResult(Uri data){
+		
+		try {
+            ContentResolver contect_resolver = mContext.getContentResolver();
+			Cursor c = contect_resolver.query(data, null, null, null, null);
+
+			if (c.moveToFirst()) {
+				String name = null;
+				String number = null;
+
+	            String id = c.getString(c.getColumnIndexOrThrow(ContactsContract.Contacts._ID));
+	                
+                Cursor phoneCur = contect_resolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
+                        ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?", new String[] { id }, null);
+                
+                if (phoneCur.moveToFirst()) {
+                    name = phoneCur.getString(phoneCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                    number = phoneCur.getString(phoneCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                }
+
+				name = c.getString(c
+						.getColumnIndexOrThrow(ContactsContract.Contacts.DISPLAY_NAME));
+				
+				Contact selected = new Contact(number, -1);
+				selected.setName(name);
+				
+				if(!c.isClosed())
+					c.close();
+				
+				return selected;
+			}
+			
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+			Log.e("IllegalArgumentException :: ", e.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+			Log.e("Error :: ", e.toString());
+		}
+		
+		return null;
 	}
 }
