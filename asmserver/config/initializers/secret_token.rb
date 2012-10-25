6cc1f701dcc0961f0161a00b5dc080674bf181a5
Logan Zhou <xorcererzc@gmail.com>
2012-10-25 19:25:49 +0800

    Removed asmserver.

--- asmserver/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Asmserver::Application.config.secret_token = '5db08f2b22407a96940351f81c7261cf6ec997c0a8d5b7716ecdfddbb2cfeae81960b67f7d566254d54ee52d65a73bd13401ce205980ce3820e7faedeb9280df'
